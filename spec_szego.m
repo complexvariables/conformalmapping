ff84c6c5b08ad8ad92d4bc3fe462e98d4ff9a148
Everett Kropf <ehkropf@gmail.com>
2014-06-15 10:32:36 +0100

    Added an internal + external map and shape fingerprint example to Szego spec file.

--- spec_szego.m
+++ spec_szego.m
@@ -76,3 +76,62 @@ subplot(1,2,1)
 plot(gd)
 subplot(1,2,2)
 plot(gdi)
+
+
+%%
+% Now we'll do a shape fingerprint. Let's use a new shape.
+clear
+G = splinep([ ...
+    0.8129 + 0.0643i; 1.1871 + 0.5673i; 1.0468 + 1.3275i
+    0.2865 + 1.6316i; -0.0643 + 1.2339i; -0.4035 + 0.6257i
+    -1.0117 + 0.8480i; -1.4211 + 0.5789i; -1.4795 - 0.2982i
+    -1.1871 - 1.1520i; -0.2865 - 0.5205i; 0.4503 - 0.7076i ...
+]);
+
+
+%%
+% Create a map for the interior and exterior of the curve. Note the use of
+% the conjugate inverse for the exterior map.
+fin = szmap(G, 0);
+g = szmap(G', 0);
+fout = @(z) 1./conj(g(1./conj(z)));
+
+%%
+% Compute the outer grid.
+gde = grid(diskex(0, 1));
+gdi = cell(1, numel(gde));
+for k = 1:numel(gde)
+  gdi{k} = fout(gde{k});
+end
+gdi = gridcurves(gdi);
+
+
+%%
+% Compute the shape fingerprint, which is just the graph of the boundary
+% correspondence angles between the shape and the unit disk with respect to the
+% interior and exterior maps.
+Sin = fin.kernel_;
+Sout = g.kernel_;
+s = (0:199)'/200;                 % Points in [0, 1) for the 
+                                  % parameterized  boundary G.
+tin = unwrap(theta(Sin, s));      % Interior boundary image angles.
+tout = unwrap(theta(Sout, s));    % Exterior boundary image angles.
+
+
+%%
+clf
+subplot(1,2,1), hold on
+plot(gdi)
+plot(fin)
+plot(G(0), 'r.')
+axis(plotbox(G))
+aspectequal
+axis off
+
+subplot(1,2,2)
+plot(tin/pi, tout/pi)
+xlabel('\theta_{inner}/\pi')
+ylabel('\theta_{outer}/\pi')
+axis([0 2 0 2])
+grid on
+aspectequal
