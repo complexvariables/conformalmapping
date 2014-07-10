9be42077a29e9437f6f9faffd1b556af9e498406
Toby Driscoll <driscoll@udel.edu>
2014-07-10 08:36:04 -0400

    Renamed run_tests to avoid path conflicts and moved some functions into private methods.

--- region.m
+++ region.m
@@ -315,4 +315,18 @@ methods(Static)
   end % checkcc
 end
 
+methods (Access=private)
+    function z = bb2z(box)
+        % BB2Z axis bounding box to vertices.
+        
+        % This file is a part of the CMToolbox.
+        % It is licensed under the BSD 3-clause license.
+        % (See LICENSE.)
+        
+        % Copyright Toby Driscoll, 2014.
+        % Written by Everett Kropf, 2014.
+        
+        z = complex(box([1, 2, 2, 1]), box([3, 3, 4, 4]));
+        z = z(:);
+    end
 end
