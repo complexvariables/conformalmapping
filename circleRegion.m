77b27e2d9ff5196b3246ea1820d16b5f0d83df4f
Everett Kropf <ehkropf@gmail.com>
2014-09-05 12:21:18 +0100

    Added circle region class.

---
+++ circleRegion.m
@@ -0,0 +1,90 @@
+classdef circleRegion < region
+%circleRegion represents a region bounded by circles
+%
+% C = circleRegion(clist)
+% Uses circles in cell array clist to create region. If clist{1} bounds the
+% following circles, the region is considered bounded. It's an unbounded
+% region otherwise.
+%
+% This class represents one of two types of regions:
+%   1. A region bounded by a circle which contains zero or more circular
+%   holes.
+%   2. An unbounded region containing one or more circular holes.
+% In either case none of the circular boundaries intersect, including
+% tangengialy.
+% A region exterior to a single circle is not currently handled by this
+% class. For this use
+%
+%     exterior(circle(c, r))
+%
+% See also circle, region.
+
+% This file is a part of the CMToolit.
+% It is licensed under the BSD 3-clause license.
+% (See LICENSE.)
+
+% Copyright Toby Driscoll, 2014.
+% E. Kropf, 2014
+
+properties
+    centers
+    radii
+end
+
+methods
+    function R = circleRegion(clist)
+        args = {};
+        if nargin
+            if ~(isa(clist, 'cell') ...
+                    && all(cellfun(@(x) isa(x, 'circle'), clist)))
+                error('CMT:InvalidArgument', ...
+                    'Expected a cell array of circles.')
+            end
+            
+            m = numel(clist);
+            cv(m,1) = 1i;
+            rv(m,1) = 0;
+            for j = 1:m
+                cv(j) = clist{j}.center;
+                rv(j) = clist{j}.radius;
+            end
+            
+            % Check bounded/unbounded.
+            isinside(1:m-1,1) = false;
+            for j = 2:m
+                if abs(cv(1) - cv(j)) + rv(j) < rv(1)
+                    isinside(j-1) = true;
+                end
+            end
+            if all(isinside)
+                args = {clist{1}, clist(2:end)};
+            elseif ~any(isinside)
+                args = {clist, 'exteriorto'};
+            else
+                error('CMT:InvalidArgument', ...
+                    ['The circles given do not define an expected region.' ...
+                    'See help.\n'])
+            end
+        end
+        
+        R = R@region(args{:});
+        if ~nargin
+            return
+        end
+        
+        R.centers = cv;
+        R.radii = rv;
+    end
+    
+    function R = inv(R)
+        % Invert circle region; inner and outer boundaries change roles.
+        C = boundary(R);
+        if hasouter(R)
+            R = region(C{2:end}, C{1});
+        else
+            R = region(C, 'interiorto');
+        end
+    end
+end
+
+end
