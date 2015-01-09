75a81cbdbd7a3c332d87e58eae3d85cd069db206
Toby Driscoll <driscoll@udel.edu>
2015-01-09 08:00:20 -0500

    Carleson grids restored.

---
+++ carlesongrid.m
@@ -0,0 +1,80 @@
+classdef carlesongrid < zgrid
+    
+    properties
+        levels = 4
+    end
+       
+
+    methods
+        function g = carlesongrid(R)
+            %TODO: Accept options.
+            g = g@zgrid(R);
+            
+            nu = 32; % Base radial line number.
+            r = 0.6*R.radius; % Base circle radius.
+
+            gc = cell(2 + g.levels + 2^(g.levels-1)*nu, 1);
+
+            % Level 0 circle.
+            gc{1} = circle(R.center,r); 
+
+            idx = 1;
+            for j = 1:g.levels
+                if j > 1
+                    nuj = 2^(j-2)*nu;
+                else
+                    nuj = nu;
+                end
+                dt = 2*pi/nuj;
+                offset = (j > 1)*dt/2;
+                for k = 1:nuj
+                    endpts = R.center + ...
+                        R.radius*[r 1-1e-8]*exp(1i*(offset + (k-1)*dt));
+                    gc{idx + k} = segment(endpts(1),endpts(2));
+                end
+
+                idx = idx + nuj + 1;
+                r = (1 + r)/2;
+                gc{idx} = circle(R.center,r*R.radius); 
+            end
+            gc{end} = circle(R.center,R.radius);
+            
+            g.dataSource = gc;
+            g.dataImage = gc;
+        end
+        
+        function g = apply(g,f)
+            src = g.dataSource;
+            for i = 1:length(src)
+                img{i} = apply(f,src{i});
+            end
+            g.dataImage = img;
+        end
+                
+        function out = plot(g)
+            src = g.dataSource;
+            img = g.dataImage;
+            newplot
+            washold = ishold;
+            
+            pref = plotset;
+            plotargs = {'linewidth',pref.gridWidth,'color',pref.gridColor};
+            
+            for i = 1:length(src)
+                h(i) = plot(img{i}); hold on
+            end
+            set(h,plotargs{:});
+            
+            if ~washold
+                axis auto
+                axis equal
+                hold off
+            end
+            
+            if nargout > 0
+                out = h;
+            end
+        end
+    end
+        
+end
--- disk.m
+++ disk.m
@@ -53,52 +53,6 @@ methods
         get(D, gridset);
     end
 
-    function gd = carlesonGrid(D, opts)
-        % Generate a basic Carleson grid. Default 5 levels.
-
-        levels = opts.numLevels;
-
-        nu = 32; % Base radial line number.
-        r = 0.6; % Base circle radius.
-
-        gc = cell(1 + levels + 2^(levels-1)*nu, 1);
-
-        % Level 0 circle.
-        ncp = 200;
-        gc{1} = r*exp(2i*pi*(0:ncp-1)'/(ncp-1));
-
-        % Base number of radial line points per unit length.
-        ppul = 200;
-
-        idx = 1;
-        for j = 1:levels
-            if j > 1
-                nuj = 2^(j-2)*nu;
-            else
-                nuj = nu;
-            end
-            ncr = ceil(j*ppul*(1 - r));
-            rln = linspace(r, 1 - 1e-8, ncr)';
-            dt = 2*pi/nuj;
-            off = (j > 1)*dt/2;
-            for k = 1:nuj
-                gc{idx + k} = rln*exp(1i*(off + (k-1)*dt));
-            end
-
-            idx = idx + nuj + 1;
-            r = (1 + r)/2;
-            np = (j+1)*ncp;
-            gc{idx} = r*exp(2i*pi*(0:np-1)'/(np-1));
-        end
-
-        gd = gridcurves(gc);
-        c = center(outer(D));
-        r = radius(outer(D));
-        if ~(c == 0 && r == 1)
-            gd = c + r*gd;
-        end
-    end
-    
     function gd = grid(D,type)
         
         if nargin < 2
@@ -119,6 +73,16 @@ methods
                 error('CMT:NotDefined', ...
                     'Grid type "%s" not recognized.', type)
         end
+    end
+    
+    function z = center(D)
+        c = boundary(D);
+        z = c.center;
+    end
+    
+    function r = radius(D)
+        c = boundary(D);
+        r = c.radius;
     end
 
 
